f02e44ee7d429bbcd0c4c0bbb40ab8c8e3d4235d
Jay Quigley <quigley.james.g@gmail.com>
2015-10-22 12:27:34 -0500

    an example tree to try to write traversals for

---
+++ 4.1_trees/traversals/example_tree.rb
@@ -0,0 +1,29 @@
+# http://rosettacode.org/wiki/Tree_traversal
+
+# http://rosettacode.org/wiki/Tree_traversal
+
+ #         1
+ #        / \
+ #       /   \
+ #      /     \
+ #     2       3
+ #    / \     /
+ #   4   5   6
+ #  /       / \
+ # 7       8   9
+
+# The correct output should look like this:
+
+# preorder:    1 2 4 7 5 3 6 8 9
+# inorder:     7 4 2 5 1 8 6 9 3
+# postorder:   7 4 5 2 8 9 6 3 1
+# level-order: 1 2 3 4 5 6 7 8 9
+
+require '../tree'
+
+four = Node.new('4',Node.new('7'))
+six = Node.new('6',Node.new('8'),Node.new('9'))
+two = Node.new('2',four,Node.new('5'))
+three = Node.new('3',six)
+@root = Node.new('1',two,three)
+# p root
