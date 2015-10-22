d6852489a602d496a512978fdfb499126c11c2d2
Jay Quigley <quigley.james.g@gmail.com>
2015-10-22 12:57:10 -0500

    a successful attempt to traverse the example tree

---
+++ 4.1_trees/traversals/preorder.rb
@@ -0,0 +1,32 @@
+require_relative 'example_tree'
+
+class Node
+
+	def preorder_traversal
+		print self.value + " "
+		self.children.each do |child|
+			child.preorder_traversal
+		end
+	end
+
+end
+
+@root.preorder_traversal
+puts
+
+
+=begin
+
+Note, I was really tempted to use this structure:
+
+if self.children.any?
+	// do stuff
+	// make a recursive call
+else
+	// do other stuff
+	// make a recursive call
+end
+
+But it turns out #each works such that if a node doesn't have any children, #each just doesn't execute.  So the method can take a pretty simple form.
+
+=end
