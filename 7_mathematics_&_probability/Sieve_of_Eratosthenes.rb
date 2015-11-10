29cfb5abfc7cb741e0a31b7909bf00fb1c2ff86f
Jay Quigley <quigley.james.g@gmail.com>
2015-11-10 13:48:50 -0600

    only need to scan through the square root of max

--- 7_mathematics_&_probability/Sieve_of_Eratosthenes.rb
+++ 7_mathematics_&_probability/Sieve_of_Eratosthenes.rb
@@ -1,13 +1,19 @@
 # Hello friend.
 # Let us find all the primes through:
-max = 100
+max = 500
 
 # Aha! I have it. We shall take an array of all natural numbers from 2 through max:
 nums = (2..max).to_a
 
-# And then, starting at the lowest number, systematically take out all numbers that are both above it (not equal to itself) and are evenly divisible by it.
+# And then, starting at the lowest number, systematically take out all numbers that are both above it (not equal to itself) and are evenly divisible by it. 
 
+# We only need to scan up through the square root of max.
+
+highest_scan = Math.sqrt(max).to_i
+p "highest_scan: #{highest_scan}"
 nums.each do |num|
+	break if num > highest_scan
+	p "num: #{num}"
 	nums.reject! {|x| x != num && x % num == 0}
 end
 p nums
