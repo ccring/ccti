20ac0e6249c458ce5c2788b0f2882406132e9d8f
Jay Quigley <quigley.james.g@gmail.com>
2015-12-07 17:02:09 -0600

    add solution to 1.3

--- 1_arrays_and_strings/1.1_all_uniq_char.rb
+++ 1_arrays_and_strings/1.1_all_uniq_char.rb
@@ -2,7 +2,7 @@ def all_uniq?(str)
 	tally = {}
 	(0..(str.length - 1)).each do |char|
 		if !tally[str[char]]
-			tally[str[char]] = 1
+			tally[str[char]] = "yup"
 		else
 			return false
 		end
@@ -10,7 +10,7 @@ def all_uniq?(str)
 	true
 end
 
-p all_uniq?('asdf')
-p all_uniq?('ass')
-p all_uniq?('a')
-p all_uniq?('aa')
+p all_uniq?('asdf') == true
+p all_uniq?('ass') == false
+p all_uniq?('a') == true
+p all_uniq?('aa') == false
---
+++ 1_arrays_and_strings/1.3_string_pemutation.rb
@@ -0,0 +1,30 @@
+def permutation?(string1, string2)
+	hash1 = {}
+	(0..string1.length - 1).each do |i|
+		char = string1[i]
+		hash1[char] ? hash1[char] += 1 : hash1[char] = 1
+	end
+	hash2 = {}
+	(0..string2.length - 1).each do |j|
+		char = string2[j]
+		return false if !hash1[char]
+		hash2[char] ? hash2[char] += 1 : hash2[char] = 1
+		return false if hash2[char] > hash1[char]
+	end
+
+	print "hash1: "
+	print hash1
+	print "hash2: "
+	print hash2
+	puts
+
+	hash1 == hash2
+end
+
+p permutation?("abcb","bbac") == true
+# p permutation?("abcb","bbac")
+
+p permutation?("abcb","bbacc") == false
+# p permutation?("abcb","bbacc")
+p permutation?("abcb","bba") == false
+# p permutation?("abcb","bba")
